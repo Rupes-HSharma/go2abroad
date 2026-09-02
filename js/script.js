0ef6cf60d31300a951cc53b6622618056a744a36
Rupesh kumar <46199853+Rupes-HSharma@users.noreply.github.com>
2026-09-02 22:37:27 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -246,7 +246,7 @@
         spaceBetween: 20,
         navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
         pagination: { el: ".swiper-pagination", clickable: true},
-        breakpoints: { 0: { slidesPerView: 1 }, 768: { slidesPerView: 2, centeredSlides: false }, 1024: { slidesPerView: 3 } }
+        breakpoints: { 0: { slidesPerView: 1 }, 768: { slidesPerView: 2, centeredSlides: false }, 1024: { slidesPerView: 4 } }
     });
 
     // Two Slide Per View - Swiper Slider Js
